42f01d162aed0f4b2cd388ec84af8d759dd6f2f0
MetzoPaino <williamjrobinson@me.com>
2014-03-29 18:43:07 +0000

    Initial Commit

---
+++ Typendium/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Typendium
+//
+//  Created by William Robinson on 29/03/2014.
+//  Copyright (c) 2014 William Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Typendium/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Typendium
+//
+//  Created by William Robinson on 29/03/2014.
+//  Copyright (c) 2014 William Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
