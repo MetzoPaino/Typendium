03a71d1f8d014f52b9baf81bfab7b2db247758df
MetzoPaino <WilliamJRobinson89@Gmail.com>
2014-05-14 13:27:45 +0100

    Made MadeIn stuff

---
+++ Typendium/MadeIn.h
@@ -0,0 +1,13 @@
+//
+//  MadeIn.h
+//  Typendium
+//
+//  Created by William Robinson on 14/05/2014.
+//  Copyright (c) 2014 William Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MadeIn : UIView
+
+@end
